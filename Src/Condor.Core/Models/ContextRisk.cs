520f1e1dc743fdf84a54f807647630f95b2babda
Luis Gonzalez <49911858+lgonzalh@users.noreply.github.com>
2026-08-12 15:30:48 -0500

    Add ContextRisk model

---
+++ Src/Condor.Core/Models/ContextRisk.cs
@@ -0,0 +1,8 @@
+namespace Condor.Core.Models;
+
+public class ContextRisk
+{
+    public string Kind { get; set; } = "";
+    public string Severity { get; set; } = "media";
+    public string Evidence { get; set; } = "";
+}
