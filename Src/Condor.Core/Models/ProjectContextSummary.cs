c9cf6d754eea97f9640eb11ea9c12566e170c17c
Luis Gonzalez <49911858+lgonzalh@users.noreply.github.com>
2026-08-12 17:29:03 -0500

    Reformat ProjectContextSummary to block namespace with explicit usings

--- Src/Condor.Core/Models/ProjectContextSummary.cs
+++ Src/Condor.Core/Models/ProjectContextSummary.cs
@@ -1,14 +1,18 @@
-namespace Condor.Core.Models;
+using System;
+using System.Collections.Generic;
 
-public class ProjectContextSummary
+namespace Condor.Core.Models
 {
-    public List<string> Languages { get; set; } = new();
-    public List<string> Frameworks { get; set; } = new();
-    public int ManifestCount { get; set; }
-    public int DocumentationCount { get; set; }
-    public bool IsGitRepository { get; set; }
-    public string? GitBranch { get; set; }
-    public bool GitIsDirty { get; set; }
-    public List<string> LastCommits { get; set; } = new();
-    public bool HasOperativeArtifacts { get; set; }
+    public class ProjectContextSummary
+    {
+        public List<string> Languages { get; set; } = new();
+        public List<string> Frameworks { get; set; } = new();
+        public int ManifestCount { get; set; }
+        public int DocumentationCount { get; set; }
+        public bool IsGitRepository { get; set; }
+        public string? GitBranch { get; set; }
+        public bool GitIsDirty { get; set; }
+        public List<string> LastCommits { get; set; } = new();
+        public bool HasOperativeArtifacts { get; set; }
+    }
 }
