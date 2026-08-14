2de97ef0ffe564bd673ac04cc9c53097d263fd76
Luis Gonzalez <49911858+lgonzalh@users.noreply.github.com>
2026-08-14 15:01:13 -0500

    Additive Images support to LlmRequest for T-011 vision

--- Src/Condor.Core/Models/LlmRequest.cs
+++ Src/Condor.Core/Models/LlmRequest.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace Condor.Core.Models;
 
 public class LlmRequest
@@ -6,4 +8,5 @@ public class LlmRequest
     public string Prompt { get; set; } = "";
     public double Temperature { get; set; } = 0.7;
     public int? MaxTokens { get; set; }
+    public List<string>? Images { get; set; }
 }
